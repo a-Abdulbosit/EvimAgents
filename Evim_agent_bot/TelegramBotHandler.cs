b439889e9c5bd96528607e4b10948b4e849a947c
a-Abdulbosit <abdulbosit0272@gmail.com>
2025-06-19 15:18:12 +0500

    db update

--- Evim_agent_bot/TelegramBotHandler.cs
+++ Evim_agent_bot/TelegramBotHandler.cs
@@ -119,13 +119,13 @@ public class TelegramBotHandler
 
         if (!_pendingLocations.TryGetValue(userId, out var session))
             return;
-        await bot.SendTextMessageAsync(chatId, query.Data);
         if (query.Data != null && query.Data.StartsWith("status_"))
         {
             var statusNum = int.Parse(query.Data.Replace("status_", ""));
             session.Location.Status = (PartnerStatus)statusNum;
 
             _pendingLocations.Remove(userId);
+            await bot.SendTextMessageAsync(chatId, query.Data);
             await _dbStorage.SaveLocationAsync(session.Location);
 
             await bot.SendTextMessageAsync(chatId,
